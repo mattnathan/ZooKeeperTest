a0836042a050314478104d9939647e673c62889f
Matt Nathan <matt.nathan+github@gmail.com>
2015-07-08 09:37:36 +0100

    Add docs and update the complex endpoint to call the echo service and the delay service.

--- brave-jaxrs2/src/main/java/com/vanti/incubator/BraveResource.java
+++ brave-jaxrs2/src/main/java/com/vanti/incubator/BraveResource.java
@@ -1,6 +1,9 @@
 package com.vanti.incubator;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.ContiguousSet;
+import com.google.common.collect.DiscreteDomain;
+import com.google.common.collect.Range;
 
 import java.util.Collection;
 import java.util.List;
@@ -29,6 +32,8 @@ import static java.util.stream.Collectors.toList;
 @Path("/brave")
 public class BraveResource {
 
+  private static final Joiner NEW_LINES = Joiner.on('\n');
+
   private final ScheduledExecutorService delayService;
   private final WebTarget fooService;
 
@@ -38,48 +43,74 @@ public class BraveResource {
     this.fooService = checkNotNull(fooService);
   }
 
+  /**
+   * Simple echo function that prints out three of whatever you give in the path.
+   *
+   * @param content The content to print out
+   * @return "{content}, {content}, {content}"
+   */
   @GET
   @Path("/echo/{text}")
   public String echo(@PathParam("text") String content) {
     return Joiner.on(", ").join(nCopies(3, content));
   }
 
-  @GET
-  @Path("/call/{howMany}")
-  public void callOtherServices(@PathParam("howMany") @DefaultValue("3") int howMany,
-                                @Suspended AsyncResponse asyncResponse) {
-    configureAsyncResponseTimeout(asyncResponse);
-    List<CompletableFuture<String>> backendResponses =
-        nCopies(howMany, "")
-            .stream()
-                // compute the requests we are about to make
-            .map((s) -> fooService.path("/brave/delay/1"))
-                // kick off all the backend requests
-            .map(this::getAsync)
-                // collect the promises of responses for those requests in a collection
-            .collect(toList());
-    Joiner newLines = Joiner.on('\n');
-
-    // once all the requests have completed
-    allOf(backendResponses)
-        // place one response per new line
-        .thenApply(newLines::join)
-            // respond to the client
-        .thenAccept(asyncResponse::resume)
-        .exceptionally((t) -> {
-          asyncResponse.resume(t);
-          return null;
-        });
-  }
-
+  /**
+   * Simple endpoint function that asynchronously waits the given number of seconds before returning.
+   *
+   * @param age           The delay in seconds
+   * @param asyncResponse Will be resumed when a result is available.
+   */
   @GET
   @Path("/delay/{age}")
-  public void asyncEcho(@PathParam("age") int age, @Suspended AsyncResponse asyncResponse) {
+  public void delay(@PathParam("age") int age, @Suspended AsyncResponse asyncResponse) {
     configureAsyncResponseTimeout(asyncResponse);
     schedule(() -> "Your request was delayed by " + age + " seconds", age, SECONDS)
         .thenAccept(asyncResponse::resume)
         .exceptionally((e) -> {
           asyncResponse.resume(e);
+          return null;
+        });
+  }
+
+  /**
+   * Complex endpoint function that calls {@code howMany} backend echo requests plus a single delay request. All this
+   * happens in parallel and the function completes asynchronously.
+   *
+   * @param howMany How many echo requests should we send
+   * @param asyncResponse Will be resumed when a result is available.
+   */
+  @GET
+  @Path("/call/{howMany}")
+  public void callOtherServices(@PathParam("howMany") @DefaultValue("3") int howMany,
+                                @Suspended AsyncResponse asyncResponse) {
+    configureAsyncResponseTimeout(asyncResponse);
+
+    // this task takes a while to complete, kick it off first so it has just that little bit longer to do its job
+    CompletableFuture<String> longTask = getAsync(fooService.path("/brave/delay/3"));
+
+    // this is the set of all values from 0 -> howMany (exclusive)
+    ContiguousSet<Integer> values = ContiguousSet.create(Range.closedOpen(0, howMany), DiscreteDomain.integers());
+    List<CompletableFuture<String>> backendResponses =
+        values.stream()
+            // compute the requests we are about to make
+            .map((val) -> fooService.path("/brave/echo/Value-" + val))
+                // kick off all the backend requests
+            .map(this::getAsync)
+                // collect the promises of responses for those requests in a collection
+            .collect(toList());
+
+    // add the long running task to the end of the triggered backend calls
+    backendResponses.add(longTask);
+
+    // once all the requests have completed
+    allOf(backendResponses)
+        // place one response per new line
+        .thenApply(NEW_LINES::join)
+            // respond to the client
+        .thenAccept(asyncResponse::resume)
+        .exceptionally((t) -> {
+          asyncResponse.resume(t);
           return null;
         });
   }
